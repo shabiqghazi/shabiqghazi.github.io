35396bccc406ea38d50eec652cf5e0fc2045e9b0
shabiqghazi <sgarkaan02@gmail.com>
2022-08-21 01:37:58 +0700

    add footer

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,9 @@
 window.addEventListener("scroll", () => {
   if (window.scrollY >= 120) {
     document.getElementById("navbar").classList.add("shadow-sm");
+    document.getElementById("toTop").classList.remove("invisible");
   } else {
+    document.getElementById("toTop").classList.add("invisible");
     document.getElementById("navbar").classList.remove("shadow-sm");
   }
 });
